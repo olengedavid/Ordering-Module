761c91bef8c1f66edc0ab687be150e16803a455c
David Otieno <olengedavid@gmail.com>
2025-04-03 21:57:19 +0300

    Create routes for retailers order

--- routes/retailer.php
+++ routes/retailer.php
@@ -8,3 +8,7 @@ Route::get('/retailers/products', [InventoryController::class, 'retailersIndex']
 Route::get('/retailers/product-search', [InventoryController::class, 'getRetailerProducts'])
         ->name('retailer.products.search');
 Route::get('retailer/regions/search', [RegionController::class, 'searchRegions']) ->name('retailer.regions.search');
+
+Route::post('retailers/create-order', [OrderController::class, 'store'])->name('retailer.orders.create');     
+Route::put('retailers/update-order/{uuid}', [OrderController::class, 'update'])->name('retailer.orders.update');
+ 
